27e86ab550bb2b18344ab917d737e73ffca303ae
nvquyet2224 <nvquyet2224@gmail.com>
2018-01-13 15:08:24 +0700

    dynamic data
    dynamic data

--- main.js
+++ main.js
@@ -1,52 +1,26 @@
-var PRECACHE = 'precache-v1';
-var RUNTIME = 'runtime';
+function loadData(url, obj) {
 
-// list the files you want cached by the service worker
-PRECACHE_URLS = [
-  'index.html',
-  './',
-  'style.css',
-  'main.js'
-];
+    $.ajax({url: url, cache: false, success: function(data) {
+        $(obj).html(data);
+    }});
+  
+}
 
 
-// the rest below handles the installing and caching
-self.addEventListener('install', event => {
-  event.waitUntil(
-     caches.open(PRECACHE).then(cache => cache.addAll(PRECACHE_URLS)).then(self.skipWaiting())
-  );
+$(document).ready(function () {
+    loadData('data-01.html',$('#box1'));
+
+    setTimeout(function(){
+        loadData('data-02.html',$('#box2'));
+    },300);
+    
+    setTimeout(function(){
+        loadData('data-03.html',$('#box3'));
+    },600);
+
+    setTimeout(function(){
+        loadData('data-04.html',$('#box4'));
+    },900);
+    
 });
 
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
-});
