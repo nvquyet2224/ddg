81ec7a11071acffc803d9762573b31f9ae4b419f
nvquyet2224 <nvquyet2224@gmail.com>
2018-01-13 16:04:44 +0700

    axa
    xaxa

--- main.js
+++ main.js
@@ -34,12 +34,12 @@ function loadData4(url, obj) {
 $(document).ready(function () {
     loadData1('data-01.html',$('#box1'));
 
-    setTimeout2(function(){
-        loadData('data-02.html',$('#box2'));
+    setTimeout(function(){
+        loadData2('data-02.html',$('#box2'));
     },100);
     
-    setTimeout3(function(){
-        loadData('data-03.html',$('#box3'));
+    setTimeout(function(){
+        loadData3('data-03.html',$('#box3'));
     },200);
 
     setTimeout(function(){
