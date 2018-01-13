c4c98e98b63263f961ef8515263a4910781fd1ab
nvquyet2224 <nvquyet2224@gmail.com>
2018-01-13 16:14:42 +0700

    end ajax
    end ajax

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 function loadData1(url, obj) {
 
-    $.ajax({url: url, cache: false, success: function(data) {
-        $(obj).html(data);
-        
-    }});
-  
 }
 function loadData2(url, obj) {
 
@@ -32,19 +27,6 @@ function loadData4(url, obj) {
 }
 
 $(document).ready(function () {
-    loadData1('data-01.html',$('#box1'));
-
-    setTimeout(function(){
-        loadData2('data-02.html',$('#box2'));
-    },100);
-    
-    setTimeout(function(){
-        loadData3('data-03.html',$('#box3'));
-    },200);
-
-    setTimeout(function(){
-        loadData4('data-04.html',$('#box4'));
-    },300);
-    
+   
 });
 
