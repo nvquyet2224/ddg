1c040fd45078c2ef01c22172c38b9afcc665ea57
nvquyet2224 <nvquyet2224@gmail.com>
2018-01-13 16:02:04 +0700

    bb
    bb

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-function loadData(url, obj) {
+function loadData1(url, obj) {
 
     $.ajax({url: url, cache: false, success: function(data) {
         $(obj).html(data);
         
-        if(url =='data-01.html'){
-           loadData('data-02.html',$('#box2'));
-        }else if(url =='data-02.html'){
-            loadData('data-03.html',$('#box3'));
-        }else if(url =='data-03.html'){
-            loadData('data-04.html',$('#box4'));
-        }
+    }});
+  
+}
+function loadData2(url, obj) {
+
+    $.ajax({url: url, cache: false, success: function(data) {
+        $(obj).html(data);
+        
+    }});
+  
+}
+function loadData3(url, obj) {
+
+    $.ajax({url: url, cache: false, success: function(data) {
+        $(obj).html(data);
+        
+    }});
+  
+}
+function loadData4(url, obj) {
+
+    $.ajax({url: url, cache: false, success: function(data) {
+        $(obj).html(data);
         
     }});
   
 }
 
 $(document).ready(function () {
-    loadData('data-01.html',$('#box1'));
+    loadData1('data-01.html',$('#box1'));
 
-    /*setTimeout(function(){
+    setTimeout2(function(){
         loadData('data-02.html',$('#box2'));
+    },100);
+    
+    setTimeout3(function(){
+        loadData('data-03.html',$('#box3'));
+    },200);
+
+    setTimeout(function(){
+        loadData4('data-04.html',$('#box4'));
     },300);
     
-    setTimeout(function(){
-        loadData('data-03.html',$('#box3'));
-    },600);
-
-    setTimeout(function(){
-        loadData('data-04.html',$('#box4'));
-    },900);
-    */
 });
 
