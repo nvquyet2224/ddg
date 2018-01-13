e43cac46e32209a073c624a11ad00fb4eff5c358
nvquyet2224 <nvquyet2224@gmail.com>
2018-01-13 15:54:51 +0700

    aax
    axax

--- main.js
+++ main.js
@@ -2,15 +2,23 @@ function loadData(url, obj) {
 
     $.ajax({url: url, cache: false, success: function(data) {
         $(obj).html(data);
+        
+        if(url =='data-01.html'){
+           loadData('data-02.html',$('#box2'));
+        }else if(url =='data-02.html'){
+            loadData('data-03.html',$('#box3'));
+        }else if(url =='data-03.html'){
+            loadData('data-04.html',$('#box4'));
+        }
+        
     }});
   
 }
 
-
 $(document).ready(function () {
     loadData('data-01.html',$('#box1'));
 
-    setTimeout(function(){
+    /*setTimeout(function(){
         loadData('data-02.html',$('#box2'));
     },300);
     
@@ -21,6 +29,6 @@ $(document).ready(function () {
     setTimeout(function(){
         loadData('data-04.html',$('#box4'));
     },900);
-    
+    */
 });
 
